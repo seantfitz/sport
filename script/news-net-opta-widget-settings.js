25f4b16d626e3bc6867491dc142594042b5182ea
seantfitz <70675153+seantfitz@users.noreply.github.com>
2022-03-25 16:50:02 +1100

    new pages, script updates.

--- script/news-net-opta-widget-settings.js
+++ script/news-net-opta-widget-settings.js
@@ -50,52 +50,110 @@ $(function(){
 			<div class="dropdown">
 				<button class="dropbtn">Football</button>
 				<div class="dropdown-content">
-					<!--<a href="football.html?competition=8&season=2021&i=${sub_id}">EPL</a>-->
-					<!--<a href="football.html?competition=214&season=2021&i=${sub_id}">A-League Men</a>-->
-					<!--<a href="#">A-League Women</a>-->
-<a href="football.html?competition=3&season=2020&i=${sub_id}">UEFA European Championship Finals</a>
-<a href="football.html?competition=4&season=2022&i=${sub_id}">World Cup</a>
-<a href="football.html?competition=5&season=2021&i=${sub_id}">Champions League</a>
-<a href="football.html?competition=6&season=2021&i=${sub_id}">UEFA Europa League</a>
-<a href="football.html?competition=8&season=2021&i=${sub_id}">English Premier League</a>
-<a href="football.html?competition=21&season=2021&i=${sub_id}">Italian Serie A</a>
-<a href="football.html?competition=22&season=2021&i=${sub_id}">German Bundesliga</a>
-<a href="football.html?competition=23&season=2021&i=${sub_id}">Spanish La Liga</a>
-<a href="football.html?competition=24&season=2021&i=${sub_id}">French Ligue 1</a>
-<a href="football.html?competition=88&season=2022&i=${sub_id}">Internationals</a>
-<a href="football.html?competition=214&season=10001&i=${sub_id}">Australian A-League Men</a>
-<a href="football.html?competition=341&season=1000022&i=${sub_id}">Asian World Cup Qualifiers</a>
-<a href="football.html?competition=378&season=2021&i=${sub_id}">Olympic Football Tournament</a>
-<a href="football.html?competition=379&season=2018&i=${sub_id}">AFC Asian Cup</a>
-<a href="football.html?competition=408&season=2022&i=${sub_id}">Women's World Cup</a>
-<a href="football.html?competition=551&season=2022&i=${sub_id}">Internationals Women</a>
-<a href="football.html?competition=578&season=2021&i=${sub_id}">Olympic Women's Tournament</a>
-<a href="football.html?competition=731&season=2021&i=${sub_id}">AFC Women's Asian Cup</a>
-<a href="football.html?competition=734&season=2019&i=${sub_id}">Olympic Women's Tournament Qualifying Asia</a>
-<a href="football.html?competition=1009&season=2019&i=${sub_id}">FFA Cup of Nations</a>
-<a href="football.html?competition=1125&season=2021&i=${sub_id}">UEFA Europa Conference League</a>
-<a href="football.html?competition=1136&season=2020&i=${sub_id}">Olympics Intercontinental Play-offs Women</a>
+					<a href="football.html?competition=379&season=2018&i=${sub_id}">AFC Asian Cup</a>
+					<a href="football.html?competition=731&season=2021&i=${sub_id}">AFC Women's Asian Cup</a>
+					<a href="football.html?competition=341&season=1000022&i=${sub_id}">Asian World Cup Qualifiers</a>
+					<a href="football.html?competition=214&season=2021&i=${sub_id}">Australian A-League Men</a>
+					<a href="football.html?competition=684&season=2021&i=${sub_id}">Australian A-League Women</a>
+					<a href="football.html?competition=5&season=2021&i=${sub_id}">Champions League</a>
+					<a href="football.html?competition=8&season=2021&i=${sub_id}">English Premier League</a>
+					<a href="football.html?competition=1009&season=2019&i=${sub_id}">FFA Cup of Nations</a>
+					<a href="football.html?competition=24&season=2021&i=${sub_id}">French Ligue 1</a>
+					<a href="football.html?competition=22&season=2021&i=${sub_id}">German Bundesliga</a>
+					<a href="football.html?competition=551&season=2022&i=${sub_id}">Internationals Women</a>
+					<a href="football.html?competition=88&season=2022&i=${sub_id}">Internationals</a>
+					<a href="football.html?competition=21&season=2021&i=${sub_id}">Italian Serie A</a>
+					<a href="football.html?competition=378&season=2021&i=${sub_id}">Olympic Football Tournament</a>
+					<a href="football.html?competition=734&season=2019&i=${sub_id}">Olympic Women's Tournament Qualifying Asia</a>
+					<a href="football.html?competition=578&season=2021&i=${sub_id}">Olympic Women's Tournament</a>
+					<a href="football.html?competition=1136&season=2020&i=${sub_id}">Olympics Intercontinental Play-offs Women</a>
+					<a href="football.html?competition=23&season=2021&i=${sub_id}">Spanish La Liga</a>
+					<a href="football.html?competition=1125&season=2021&i=${sub_id}">UEFA Europa Conference League</a>
+					<a href="football.html?competition=6&season=2021&i=${sub_id}">UEFA Europa League</a>
+					<a href="football.html?competition=3&season=2020&i=${sub_id}">UEFA European Championship Finals</a>
+					<a href="football.html?competition=408&season=2022&i=${sub_id}">Women's World Cup</a>
+					<a href="football.html?competition=4&season=2022&i=${sub_id}">World Cup</a>
 				</div>
 			</div>
+			
 			<div class="dropdown">
 				<button class="dropbtn">Basketball</button>
-				<div class="dropdown-content"></div>
+				<div class="dropdown-content">
+					<a href="basketball.html?competition=5wdi9kmiurd2rjx2f7ew92wm6&season=4sjxxlpzq4fgth238gnebn3tg&i=${sub_id}">NBL</a>
+				</div>
 			</div>
+			
 			<div class="dropdown">
 				<button class="dropbtn">Tennis</button>
 				<div class="dropdown-content"></div>
 			</div>
 			<div class="dropdown">
 				<button class="dropbtn">Golf</button>
-				<div class="dropdown-content"></div>
+				<div class="dropdown-content">
+					<a href="golf.html?competition=3c5msl8moic00eu49b06aumsp&season=405fzvlo157c392mbz8odw9p0&i=${sub_id}">PGA Tour</a>
+				</div>
 			</div>
 			<div class="dropdown">
 				<button class="dropbtn">Cricket</button>
-				<div class="dropdown-content"></div>
+				<div class="dropdown-content">
+					<a href="cricket.html?competition=2722&season=na&i=${sub_id}">England v India Test Series 2021/2022</a>
+					<a href="cricket.html?competition=2780&season=na&i=${sub_id}">IPL 2021</a>
+					<a href="cricket.html?competition=2781&season=na&i=${sub_id}">ICC World Test Championship Final 2021</a>
+					<a href="cricket.html?competition=2784&season=na&i=${sub_id}">West Indies v England Twenty20 Series 2022</a>
+					<a href="cricket.html?competition=2785&season=na&i=${sub_id}">West Indies v England Test Series 2022</a>
+					<a href="cricket.html?competition=2813&season=na&i=${sub_id}">Australia v England Test Series 2021/2022</a>
+					<a href="cricket.html?competition=2816&season=na&i=${sub_id}">Australia v Sri Lanka Twenty20 Series 2022</a>
+					<a href="cricket.html?competition=2831&season=na&i=${sub_id}">Weber Women's Big Bash League 2021</a>
+					<a href="cricket.html?competition=2832&season=na&i=${sub_id}">KFC T20 Big Bash League 2021/2022</a>
+					<a href="cricket.html?competition=2852&season=na&i=${sub_id}">ICC Men's T20 World Cup 2021</a>
+					<a href="cricket.html?competition=2856&season=na&i=${sub_id}">England v New Zealand Test Series 2022</a>
+					<a href="cricket.html?competition=2857&season=na&i=${sub_id}">England v India Twenty20 Series 2022</a>
+					<a href="cricket.html?competition=2858&season=na&i=${sub_id}">England v India ODI Series 2022</a>
+					<a href="cricket.html?competition=2859&season=na&i=${sub_id}">England v South Africa ODI Series 2022</a>
+					<a href="cricket.html?competition=2860&season=na&i=${sub_id}">England v South Africa Twenty20 Series 2022</a>
+					<a href="cricket.html?competition=2861&season=na&i=${sub_id}">England v South Africa Test Series 2022</a>
+					<a href="cricket.html?competition=2863&season=na&i=${sub_id}">South Africa v India Test Series 2021/2022</a>
+					<a href="cricket.html?competition=2864&season=na&i=${sub_id}">South Africa v India ODI Series 2022</a>
+					<a href="cricket.html?competition=2868&season=na&i=${sub_id}">South Africa v Bangladesh ODI Series 2022</a>
+					<a href="cricket.html?competition=2869&season=na&i=${sub_id}">South Africa v Bangladesh Test Series 2022</a>
+					<a href="cricket.html?competition=2878&season=na&i=${sub_id}">India v West Indies ODI Series 2022</a>
+					<a href="cricket.html?competition=2879&season=na&i=${sub_id}">India v West Indies Twenty20 Series 2022</a>
+					<a href="cricket.html?competition=2880&season=na&i=${sub_id}">India v Sri Lanka Test Series 2022</a>
+					<a href="cricket.html?competition=2881&season=na&i=${sub_id}">India v Sri Lanka Twenty20 Series 2022</a>
+					<a href="cricket.html?competition=2882&season=na&i=${sub_id}">India v South Africa Twenty20 Series 2022</a>
+					<a href="cricket.html?competition=2886&season=na&i=${sub_id}">Netherlands v England ODI Series 2022</a>
+					<a href="cricket.html?competition=2903&season=na&i=${sub_id}">Pakistan v Australia Test Series 2022</a>
+					<a href="cricket.html?competition=2904&season=na&i=${sub_id}">Pakistan v Australia ODI Series 2022</a>
+					<a href="cricket.html?competition=2905&season=na&i=${sub_id}">Pakistan v Australia Only Twenty20 2022</a>
+					<a href="cricket.html?competition=2908&season=na&i=${sub_id}">New Zealand v Bangladesh Test Series 2022</a>
+					<a href="cricket.html?competition=2911&season=na&i=${sub_id}">New Zealand v South Africa Test Series 2022</a>
+					<a href="cricket.html?competition=2913&season=na&i=${sub_id}">New Zealand v Netherlands Only Twenty20 2022</a>
+					<a href="cricket.html?competition=2914&season=na&i=${sub_id}">New Zealand v Netherlands ODI Series 2022</a>
+					<a href="cricket.html?competition=2917&season=na&i=${sub_id}">West Indies v Ireland ODI Series 2022</a>
+					<a href="cricket.html?competition=2920&season=na&i=${sub_id}">Afghanistan v Netherlands ODI Series in Qatar 2022</a>
+					<a href="cricket.html?competition=2922&season=na&i=${sub_id}">Sri Lanka v Zimbabwe ODI Series 2022</a>
+					<a href="cricket.html?competition=2934&season=na&i=${sub_id}">New Zealand in England Tour Matches 2022</a>
+					<a href="cricket.html?competition=2938&season=na&i=${sub_id}">South Africa in England Tour Matches 2022</a>
+					<a href="cricket.html?competition=2939&season=na&i=${sub_id}">India in England Tour Matches 2022</a>
+					<a href="cricket.html?competition=2940&season=na&i=${sub_id}">ICC Men's T20 World Cup 2022</a>
+					<a href="cricket.html?competition=2945&season=na&i=${sub_id}">Bangladesh v Afghanistan ODI Series 2022</a>
+					<a href="cricket.html?competition=2946&season=na&i=${sub_id}">Bangladesh v Afghanistan Twenty20 Series 2022</a>
+					<a href="cricket.html?competition=2953&season=na&i=${sub_id}">Netherlands v West Indies ODI Series 2022</a>
+					<a href="cricket.html?competition=2954&season=na&i=${sub_id}">Ireland v India Twenty20 Series 2022</a>
+					<a href="cricket.html?competition=2955&season=na&i=${sub_id}">Ireland v New Zealand ODI Series 2022</a>
+					<a href="cricket.html?competition=2956&season=na&i=${sub_id}">Ireland v New Zealand Twenty20 Series 2022</a>
+					<a href="cricket.html?competition=2957&season=na&i=${sub_id}">Ireland v South Africa Twenty20 Series in England 2022</a>
+					<a href="cricket.html?competition=2960&season=na&i=${sub_id}">IPL 2022</a>
+					<a href="cricket.html?competition=2966&season=na&i=${sub_id}">Nepal v Papua New Guinea ODI Series 2022</a>
+				</div>
 			</div>
 			<div class="dropdown">
 				<button class="dropbtn">Motorsport</button>
-				<div class="dropdown-content"></div>
+				<div class="dropdown-content">
+					<a href="motorsport.html?competition=1m5x4n07f2kqkiczebja5etiq&season=3d2cd3uj88qgdlt4lyhr0cis4&i=${sub_id}">FIA F1 World Championship</a>
+					<a href="motorsport.html?competition=2e554vbpw7g0ykhl0rziuabxl&season=3j534o30hn9scfadkb0v9hkb8&i=${sub_id}">FIM MotoGP World Championship</a>
+					<a href="motorsport.html?competition=2ecsbewwstw5jn9h7kpv2joix&season=e9p0ngu4n2do5agfk6wt23f9w&i=${sub_id}">V8 Supercars</a>
+				</div>
 			</div>
 			<div class="dropdown">
 				<button class="dropbtn">US Sports</button>
@@ -107,14 +165,31 @@ $(function(){
 			</div>
 			<div class="dropdown">
 				<button class="dropbtn">Rugby Union</button>
-				<div class="dropdown-content"></div>
+				<div class="dropdown-content">
+					<a href="rugbyunion.html?competition=3&season=2023&i=${sub_id}">International</a>
+					<a href="rugbyunion.html?competition=205&season=2022&i=${sub_id}">Super Rugby Pacific</a>
+					<a href="rugbyunion.html?competition=209&season=2022&i=${sub_id}">Six Nations</a>
+					<a href="rugbyunion.html?competition=210&season=2024&i=${sub_id}">Rugby World Cup</a>
+					<a href="rugbyunion.html?competition=214&season=2022&i=${sub_id}">The Rugby Championship</a>
+					<a href="rugbyunion.html?competition=221&season=2021&i=${sub_id}">British & Irish Lions</a>
+					<a href="rugbyunion.html?competition=249&season=2022&i=${sub_id}">Women's Rugby World Cup</a>
+				</div>
 			</div>
 			<div class="dropdown">
 				<button class="dropbtn">Rugby League</button>
-				<div class="dropdown-content"></div>
+				<div class="dropdown-content">
+					<a href="rugbyleague.html?competition=3&season=2022&i=${sub_id}">National Rugby League</a>
+					<a href="rugbyleague.html?competition=6&season=2021&i=${sub_id}">State of Origin</a>
+					<a href="rugbyleague.html?competition=86&season=2021&i=${sub_id}">NRL Women's Premiership</a>
+					<a href="rugbyleague.html?competition=87&season=2021&i=${sub_id}">Women's State of Origin</a>
+				</div>
 			</div>
 			<div class="dropdown">
-				<button class="dropbtn">AFL</button></div>
+				<button class="dropbtn">AFL</button>
+				<div class="dropdown-content">
+					<a href="afl.html?competition=11686&season=2022&i=${sub_id}">AFL</a>
+					<a href="afl.html?competition=11595&season=2022&i=${sub_id}">AFLW</a>
+				</div>
 			</div>
 		</div>
 	`)
@@ -253,6 +328,35 @@ if(document.location.href.indexOf('football.html') >= 0){//check that this is th
 	}
 }
 /*FOOTBALL*/
+
+/*BASKETBALL*/
+if(document.location.href.indexOf('basketball.html') >= 0){
+	switch(query['competition']){
+		case '3':
+		case '3':
+
+		let fixturesTopLeft = document.getElementsByClassName('fixtures-top-left')[0]
+		let matchSummary = document.getElementById('match-centre-top-left')
+		let standingsWidget = document.getElementById('standings-widget')
+
+		fixturesTopLeft.innerHTML = (`
+			<opta-widget widget="scoreboard" 
+			competition="${query['competition']}" 
+			season="${query['season']}" 
+			template="normal" live="true" show_venue="true" grouping="date" show_grouping="true" show_date_picker="true" navigation="tabs_scroll" default_nav="1" start_on_current="true" order_by="date_ascending" away_team_first="false" show_crests="true" date_format="dddd D MMMM YYYY" time_format="HH:mm" month_date_format="MMMM" competition_naming="full" team_naming="full" pre_match="false" show_live="true" show_logo="false" show_title="false" breakpoints="400" sport="basketball"></opta-widget>
+		`)
+
+		standingsWidget.innerHTML=(`
+			<opta-widget widget="standings" 
+			competition="5wdi9kmiurd2rjx2f7ew92wm6" 
+			season="4sjxxlpzq4fgth238gnebn3tg" 
+			template="normal" navigation="tabs" default_nav="1" show_categories="all" show_rank="true" show_key="true" show_crests="true" sorting="true" team_naming="full" show_logo="false" show_title="true" breakpoints="460" sport="basketball"></opta-widget>
+		`)
+
+		break;
+	}
+}
+/*BASKETBALL*/
 
 /*MATCH CENTRE STUFF*/
 if(document.location.href.indexOf('match.html') >= 0 || document.location.href.indexOf('test2.html') >= 0){//check that this is a match centre page
