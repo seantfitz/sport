082bfb1fb2cbefa8cbce59eeccddbd1209158df1
seantfitz <70675153+seantfitz@users.noreply.github.com>
2022-06-13 13:34:58 +1000

    iFrame resizing for US sports

---
+++ us-sports/nfl/scoreboard.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale = 1, maximum-scale = 1.0, user-scalable=no" />
+	<link rel="stylesheet" type="text/css" href="//widgets-assets.stats.com/css/widgets-football-scoreboard.min.css">
+	<title>nfl</title>
+</head>
+<body style="width: 100%; height: 100vh; margin: 0; background-color: #fc0;">
+<div id="shs-football-scoreboard" data-shs-league="nfl" data-shs-client-id="news"></div>
+<script type="text/javascript" src="https://widgets-assets.stats.com/js/football-scoreboard.standalone.min.js"></script>
+</body>
+</html>
